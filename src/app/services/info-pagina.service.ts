eefa6963951bd232a9d8b169257707e7fc3cf45f
Eze Di Giulio <eze.digiulio@gmail.com>
2019-09-16 20:20:51 -0300

    seccion 5 terminada

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,32 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any = [];
+
   constructor( private http: HttpClient) {
-    console.log('servicio infoPagina listo');
-      // leer json
-    this.http.get('assets/data/data-pagina.json')
-          .subscribe( (resp: InfoPagina) => {
-            this.cargada = true;
-            this.info = resp;
-            // console.log(resp);
-            console.log(resp);
-          });
+
+    this.cargarInfo();
+    this.cargarEquipo();
 
   }
 
+  private cargarInfo(){
+      // leer json
+      this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+  }
 
+  private cargarEquipo(){
+      // leer json
+      this.http.get('https://angular-html-a33ad.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+
+        this.equipo = resp;
+        // console.log(resp);
+        console.log(resp);
+      });
+  }
 }
